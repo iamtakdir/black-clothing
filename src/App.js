5c5811734fc7570ed7fe37c56ae7c1aa1ecfdc1a
Takdirul Islam Shishir <39631235+iamtakdir@users.noreply.github.com>
2022-06-22 23:54:08 +0600

    Signin component created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from "./routes/home/home.components";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/navigation/navigation.components";
 
+import SignIn from "./routes/sign-in/sign-in.components";
+
 const Shop = () => {
   return <h1>I am the shop</h1>;
 };
@@ -12,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
